58ac8cb381546abd24dc073e04090a93f07cfdd6
Maria Luisa <araujocostamalu@gmail.com>
2023-07-10 11:39:24 -0300

    chore: adjusting connection

--- src/infra/connection.ts
+++ src/infra/connection.ts
@@ -3,11 +3,19 @@ import { Pool } from 'pg';
 
 dotenv.config();
 
+// export const pool = new Pool({
+//   user: "root",
+//   host: "127.0.0.1",
+//   database: "gerenciamentodebolsas",
+//   password: "example",
+//   port: 5432,
+// });
+
 export const pool = new Pool({
-  user: "postgres",
-  host: "127.0.0.1",
-  database: "gerenciamentodebolsas",
-  password: "1234",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASS,
+  port: Number(process.env.DB_PORT),
 });
 
